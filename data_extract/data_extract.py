a456e2a6b47128a78fbee21e5ce713aef7a41de6
Bruno28-02 <bruno.ferraz@germinare.org.br>
2026-02-12 11:36:17 -0300

    feat(data_extract): updating methods call

--- data_extract/data_extract.py
+++ data_extract/data_extract.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+from methods import get_token, get_auth_header,get_artists,get_track,get_track_infos
 import os
 
 load_dotenv()
@@ -6,3 +7,10 @@ load_dotenv()
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 
+
+
+token = get_token(client_id=client_id,client_secret=client_secret)
+auth_header = get_auth_header(token=token)
+print(auth_header)
+
+track = get_track_infos('4uLU6hMCjMI75M1A2tKUQC',auth_header)
