d4bbd736e7797026a1071c9eb76e16831736e185
Bruno28-02 <bruno.ferraz@germinare.org.br>
2026-02-19 13:16:53 -0300

    feat(data_extract): creating an orqustrator to organize the methods calls

--- data_extract/data_extract.py
+++ data_extract/data_extract.py
@@ -1,17 +1,19 @@
 from dotenv import load_dotenv
-from methods import get_token_spotify, get_auth_header_spotify,get_track_spotify
+from methods.methods import get_token_spotify, get_auth_header_spotify, get_track_spotify, get_track_id_deezer, classifying_number_parameters_aubio, classifying_song_aubio
 import os
 
 load_dotenv()
 
-client_id = os.getenv("CLIENT_ID")
-client_secret = os.getenv("CLIENT_SECRET")
+spotify_client_id = os.getenv("SPOTIFY_CLIENT_ID")
+spotify_client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
 
-
-
-token = get_token_spotify(client_id=client_id,client_secret=client_secret)
-auth_header = get_auth_header_spotify(token=token)
-
-track = get_track_spotify('4uLU6hMCjMI75M1A2tKUQC',auth_header)
-
-print(track)
+def orquestrator():
+    token = get_token_spotify(client_id=spotify_client_id,client_secret=spotify_client_secret)
+    auth_header = get_auth_header_spotify(token=token)
+    track = get_track_spotify('40s4PAZc1lqzGk0LL1mMZh',auth_header)
+    track_artist = track["artist"]
+    track_name = track["song"]
+    preview = get_track_id_deezer(track_name,track_artist)
+    metrics = classifying_number_parameters_aubio(preview)
+    classification = classifying_song_aubio(metrics)
+    return classification
